43b9ec2c07a6ad2b4f04ed5818f25a94bbcc7bd6
catherine-newman <3197941+catherine-newman@users.noreply.github.com>
2023-07-18 14:01:14 +0100

    remove functionality to vote on articles from article list

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,10 +1,8 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { formatDate } from "../utilities/formatDate";
-import { patchArticle } from "../api";
-import { toast } from "react-toastify";
-import ArticleVote from "./ArticleVote";
-import { useState } from "react";
+import { HiOutlineArrowsUpDown } from "react-icons/hi2";
+import { IconContext } from "react-icons";
 
 const Card = styled.div`
   background-color: #ffffff;
@@ -29,62 +27,43 @@ const CardFooter = styled.div`
   justify-content: space-evenly;
 `;
 
+const VotesDiv = styled.div`
+  display: flex;
+  gap: 0.5em;
+  align-items: center;
+
+  svg {
+    cursor: pointer;
+  }
+`;
+
 const ArticleCard = ({ article }) => {
-  const [voteCount, setVoteCount] = useState(article.votes);
-
-  const handleUpVote = () => {
-    const newCount = voteCount + 1;
-    setVoteCount(newCount);
-    (async () => {
-      try {
-        const res = await patchArticle(article.article_id, 1);
-        setVoteCount(res.votes);
-      } catch (err) {
-        toast.error("Oops! Something went wrong...");
-        setVoteCount(voteCount);
-      }
-    })();
-  };
-
-  const handleDownVote = () => {
-    const newCount = voteCount - 1;
-    setVoteCount(newCount);
-    (async () => {
-      try {
-        const res = await patchArticle(article.article_id, -1);
-        setVoteCount(res.votes);
-      } catch (err) {
-        toast.error("Oops! Something went wrong...");
-        setVoteCount(voteCount);
-      }
-    })();
-  };
-
   return (
-    <Card>
-      <CardHeader>
-        <div>
-          {article.author} {formatDate(article.created_at)}
-        </div>
-        <div>{article.topic}</div>
-      </CardHeader>
-      <div>
-        <Link to={`/${article.topic}/${article.article_id}`}>
-          <h2>{article.title}</h2>
+    <IconContext.Provider value={{ style: { fontSize: "1.3em" } }}>
+      <Card>
+        <CardHeader>
           <div>
-            <img src={article.article_img_url} />
+            {article.author} {formatDate(article.created_at)}
           </div>
-        </Link>
-      </div>
-      <CardFooter>
-        <ArticleVote
-          votes={voteCount}
-          handleUpVote={handleUpVote}
-          handleDownVote={handleDownVote}
-        />
-        <div>{article.comment_count}</div>
-      </CardFooter>
-    </Card>
+          <div>{article.topic}</div>
+        </CardHeader>
+        <div>
+          <Link to={`/${article.topic}/${article.article_id}`}>
+            <h2>{article.title}</h2>
+            <div>
+              <img src={article.article_img_url} />
+            </div>
+          </Link>
+        </div>
+        <CardFooter>
+          <VotesDiv>
+            <HiOutlineArrowsUpDown />
+            {article.votes}
+          </VotesDiv>
+          <div>{article.comment_count}</div>
+        </CardFooter>
+      </Card>
+    </IconContext.Provider>
   );
 };
 
